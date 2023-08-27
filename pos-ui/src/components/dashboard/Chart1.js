6b22ffe5dc1e6b069a520070f94352cd948406df
aaqure786 <88874833+aaqure786@users.noreply.github.com>
2023-08-27 08:02:02 +0500

    Charts without buttons

--- pos-ui/src/components/dashboard/Chart1.js
+++ pos-ui/src/components/dashboard/Chart1.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import moment from 'moment';
-import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, CartesianGrid } from 'recharts'
+import { LineChart, Line, XAxis, YAxis, ResponsiveContainer, CartesianGrid, Tooltip, Legend } from 'recharts'
 
 const Chart1 = () => {
-    const startDate = moment(moment().subtract(30, 'days')).format("DD-MMMM-YYYY")
+    const startDate = moment(moment().subtract(29, 'days')).format("DD-MMMM-YYYY")
 
     const endDate = moment().format("DD-MMMM-YYYY")
     function enumerateDaysBetweenDates(startDate, endDate) {
         let chartData = []
+        let n= 2
+
         while (moment(startDate) <= moment(endDate)) {
-            let val ={ value: "0", date:startDate}
+            let val = { value: n, date: startDate }
             chartData.push(val);
+            n += 2
             startDate = moment(startDate).add(1, 'days').format("DD-MMMM-YYYY");
         }
         return chartData;
@@ -42,29 +45,30 @@ const Chart1 = () => {
     // ]
 
     return (
-        <div className=' w-[96%] mx-[2%] shadow-md  shadow-gray-400 h-[500px] border-t-[2px] border-blue-600 rounded-xl'>
-            <h1 className='text-xl font-semibold text-start p-5'>Sales Last 30 Days</h1>
-            <div className='mt-2 w-full'>
-                <ResponsiveContainer  width={"100%"}   aspect={3}>
-                    <LineChart width={"100%"}  height={"400px"} data={datesArray} >
-                        <Line dataKey="value" />
-                        <YAxis />
-                        <CartesianGrid strokeDasharray="3 3"/>
-                        <XAxis dataKey="date" interval={'preserveStartEnd'} 
-                        angle={-45}
-                         padding="no-gap"
-                          minTickGap={-100}
-                           dy={30}
-                           tick={{fontSize:"11"}}
-                        />
-                        
-                        
-                    </LineChart>
-                </ResponsiveContainer>
+        <ResponsiveContainer width={"100%"} height={"80%"}>
+            <LineChart width={"100%"} height={"400px"} data={datesArray}   margin={{ left: 30, right: 30 }}>
+                <YAxis label={{ value: 'Total Sales (PKR)', angle: -90, position: 'insideLeft' }} />
 
+                
 
-            </div>
-        </div>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" interval={'preserveStartEnd'}
+                    angle={-45}
+                    padding={{left:25, right:25}}
+                    minTickGap={-100}
+                    dy={30}
+                    dx={-30}
+                    tick={{ fontSize: "11" }}
+                    height={70}
+                    
+
+                />
+                 <Tooltip />
+                <Legend  verticalAlign='top' height={30}/>
+                <Line dataKey="value" />
+
+            </LineChart>
+        </ResponsiveContainer>
     )
 }
 
